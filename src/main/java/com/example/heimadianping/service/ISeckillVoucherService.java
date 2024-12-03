72d600dc13cfc24f4b3eff4335e50083609ec20d
sy <3078567417@qq.com>
2024-12-03 09:36:00 +0800

    "."

--- src/main/java/com/example/heimadianping/service/ISeckillVoucherService.java
+++ src/main/java/com/example/heimadianping/service/ISeckillVoucherService.java
@@ -8,7 +8,7 @@ import com.example.heimadianping.entity.SeckillVoucher;
  * 秒杀优惠券表，与优惠券是一对一关系 服务类
  * </p>
  *
- * @author 虎哥
+ * @author sy
  * @since 2022-01-04
  */
 public interface ISeckillVoucherService extends IService<SeckillVoucher> {
